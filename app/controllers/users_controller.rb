81035b276287208ba642cd8a5e4e1d7430cb1a73
Rails Udemy Course <mario@CRmomarioa12.microfocus.com>
2020-02-26 12:53:56 -0600

    Add root user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   before_action :set_user, only: [:edit, :update, :show]
-  before_action :require_same_user, only: [:edit,:update]
+  before_action :require_same_user, only: [:edit,:update, :destroy]
   def index
     @users = User.paginate(page: params[:page],per_page: 2)
   end
@@ -34,6 +34,13 @@ class UsersController < ApplicationController
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 3)
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = "User and all articles had been deleted"
+    redirect_to users_path
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
@@ -45,7 +52,7 @@ class UsersController < ApplicationController
 end
 
 def require_same_user
-  if current_user != @user
+  if current_user != @user and !current_user.admin?
     flash[:danger] = "You can only edit or delete your own user"
     redirect_to root_path
   end
